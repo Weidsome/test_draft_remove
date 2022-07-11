46584858acf53a0966da7924f109e0bbf5bb3734
Dmitry <havriemetal@yandex.ru>
2022-07-11 05:47:48 +0300

    New files added

---
+++ test_copy.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let description = "This is a test file for my Git class"
+
+var a = [Int:Int]()
---
+++ test_copy_to_ignore.playground/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+let description = "This is a test file for my Git class"
